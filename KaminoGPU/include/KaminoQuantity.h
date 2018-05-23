ae2e3b0c48ec875284e45f472a7f44597fbd8f45
AIPC <grillnov@gmail.com>
2018-05-23 13:11:36 -0400

    Decided the gpu memory layout

--- KaminoGPU/include/KaminoQuantity.h
+++ KaminoGPU/include/KaminoQuantity.h
@@ -23,10 +23,16 @@ private:
 
 	/* Initial buffer at client side */
 	fReal* cpuBuffer;
-	/* Double buffer at server side */
-	cudaArray* gpuThisStep;
-	cudaArray* gpuNextStep;
+	/* Double pitch buffer at server side */
+	fReal* gpuThisStep;
+	size_t thisStepPitch;
+	fReal* gpuNextStep;
+	size_t nextStepPitch;
 
+	table2D tex;
+	/* Convert to texture */
+	void setTextureParams();
+	cudaChannelFormatDesc desc;
 	/* Get index */
 	//size_t getIndex(size_t phi, size_t theta);
 
@@ -39,10 +45,11 @@ public:
 
 	/* Swap the GPU buffer */
 	void swapGPUBuffer();
-	/* Convert to texture */
-	void createTexture();
 	/* Copy the CPU end part to GPU */
 	void copyToGPU();
+	/* Bind the texture */
+	void bindTexture();
+	void unbindTexture();
 
 	/* Get its name */
 	std::string getName();
@@ -59,6 +66,12 @@ public:
 	/* Get the offset */
 	fReal getPhiOffset();
 	fReal getThetaOffset();
+	fReal* getGPUThisStep();
+	fReal* getGPUNextStep();
+
+	table2D getTex();
+	size_t getThisStepPitch();
+	size_t getNextStepPitch();
 
 	static cudaChannelFormatDesc channelFormat;
 };
